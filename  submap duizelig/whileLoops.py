cac5a35e5b7dced9a7ffe8acd35f242c17cefe63
Jason <99057964@mydavinci.nl>
2021-11-18 14:19:07 +0100

    Bezig met de while loop lab.

---
+++  submap duizelig/whileLoops.py
@@ -0,0 +1,18 @@
+#De eerste opdracht.
+teller = 30
+while teller > 0:
+    print(teller)
+    teller = teller - 1
+
+#De tweede opdracht.
+uren = 0
+while uren < 12:
+    if uren < 6:
+        print(str(uren) + ' am')
+    else:
+        print(str(uren) + ' pm')
+    uren += 1  
+
+getal = 20
+
+
